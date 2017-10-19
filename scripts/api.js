9fa2dd05bf81e9d5fce9eaaf9cd24acef3a5b126
Martin Petkov <snutz@mail.bg>
2017-10-19 13:22:43 +0200

    Added the adverts functionality

--- scripts/api.js
+++ scripts/api.js
@@ -1,5 +1,18 @@
 (function () {
-    // Mock repository
+    // Mock repository    
+    let adverts = [
+        {
+            _id: 0,
+            _acl: {
+                creator: 0
+            },
+            title: "XBoss 1080",
+            publisher: "Pesho",
+            datePublished: "2017-06-04",
+            price: 100
+        }
+    ];
+
     let users = [
         {
             _kmd: {
@@ -71,6 +84,24 @@
                         };
                         users.push(user);
                         this.responseText = user;
+                    } else {
+                        this.status = 403;
+                        this.responseText = "You are not authorized";
+                    }
+                }
+            };
+        }
+    });
+
+
+    // Loading of adverts
+    $.mockjax(function (requestSettings) {
+        if (requestSettings.url==="https://mock.api.com/appdata/kid_rk/adverts" &&
+            requestSettings.method === "GET") {
+            return {
+                response: function (origSettings) {
+                    if (requestSettings.headers["Authorization"].includes("Kinvey mock_token")) {
+                        this.responseText = adverts;
                     } else {
                         this.status = 403;
                         this.responseText = "You are not authorized";
